02e33b21399a012685d50ed04a44af30e0f370e0
hialmar <torguet@gmail.com>
2020-03-02 13:53:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hialmar-crc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hialmar-crc)
---
+++ src/app/app.component.ts
@@ -0,0 +1,118 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  a = '100110000';
+  b = '101';
+
+  constructor() {
+    this.divide(this.toArray(this.a), this.toArray(this.b));
+  }
+
+  // All algos are from https://asecuritysite.com/comms/crc_div
+
+  toArray(p: string) : number[] {
+    let tab = [];
+    let tabStr = p.split('');
+    for(let i = 0; i < tabStr.length; i++) {
+      tab.push(parseInt(tabStr[i]));
+    }
+    return tab;
+  }
+
+  toSimpleString(p: number[]) : string {
+    let s = '';
+    for(let i = 0; i < p.length; i++) {
+      s += p[i];
+    }
+    return s;
+  }
+
+  toPolyString(p: number[]) : string {
+    let s = '';
+    const nobits = p.length;
+    for(let i = 0; i < nobits-2; i++) {
+      if(p[i]===1) {
+        if(s.length === 0) {
+          s += 'x^'+(nobits-i-1);
+        } else {
+          s += '+x^'+(nobits-i-1);
+        }
+      }
+    }
+    if(nobits >= 2 && p[nobits-2] === 1) {
+      if(s.length === 0) {
+          s += 'x';
+        } else {
+          s += '+x';
+        }
+    }
+    if(nobits >= 1 && p[nobits-1] === 1) {
+      if(s.length === 0) {
+          s += '1';
+        } else {
+          s += '+1';
+        }
+    }
+    return s;
+  }
+
+  quotient = [];
+  remainder = [];
+
+  generateDashes(nb: number): string {
+    let s = '';
+    for(let i=0; i<nb; i++)
+      s += '-'
+    return s;
+  }
+  generateZeroes(nb: number): string {
+    let s = '';
+    for(let i=0; i<nb; i++)
+      s += '0'
+    return s;
+  }
+
+  working = '';
+  divide(a: number[], b: number[]) {
+    this.working=this.toSimpleString(a)+'\n';
+    let addspace='';
+    this.quotient = [];
+    this.remainder = [];
+
+    while (b.length <= a.length) {
+      if(a[0] === 1) {
+        a.shift();
+        for(let j=0; j<b.length-1; j++) {
+          a[j] ^= b[j+1];
+        }
+        if(a.length>0) {
+          this.working += addspace+this.toSimpleString(b)+'\n';
+          this.working += addspace+this.generateDashes(b.length)+'\n';
+          addspace+= ' ';
+          this.working += addspace+this.toSimpleString(a)+'\n';
+          this.quotient.push(1);
+        }
+      } else {
+        a.shift();
+        this.working += addspace+this.generateZeroes(b.length)+"\n"
+			  this.working += addspace+this.generateDashes(b.length)+'\n';
+			  addspace+=' ';
+			  this.working += addspace+this.toSimpleString(a)+'\n';
+        this.quotient.push(0);
+      }
+    }
+    console.log("Result "+this.toSimpleString(this.quotient));
+    this.remainder = a;
+    console.log("Remainder "+this.toSimpleString(a));
+
+    console.log("Working : ");
+    console.log(this.working);
+  }
+
+}
+
